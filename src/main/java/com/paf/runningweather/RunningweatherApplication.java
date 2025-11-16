83e3e8b3f6346684eec111325d5eef88cc1b6dfa
hannah-fitz <hannah.fitz2608@gmail.com>
2025-11-16 11:14:23 +0100

    Initial commit

---
+++ src/main/java/com/paf/runningweather/RunningweatherApplication.java
@@ -0,0 +1,13 @@
+package com.paf.runningweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paf/runningweather/RunningweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paf.runningweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
